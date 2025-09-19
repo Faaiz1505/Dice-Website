106a99a2ff7aea8e405ae79442a85d891eecc254
NafasHub <nasirudeenfaaiz755@gmail.com>
2025-09-19 10:59:51 +0100

    initial commit

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ if (randomNumber1 > randomNumber2) {
 } else if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 } else {
-    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = "Yooh, It's a Draw!";
 }
